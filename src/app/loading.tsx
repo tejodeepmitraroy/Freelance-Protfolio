7b2595ca8c2fac29161a75b77cedeb045db9aad1
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2023-12-26 01:12:12 +0530

    Update Loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-white text-[#C1AEFC]">
+      Tejodeep Mitra Roy
+    </div>
+  );
+};
+
+export default loading;
