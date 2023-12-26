8ad4932ef973f11b72e2d241ca0fc70d52504e16
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2023-12-26 11:15:13 +0530

    error

--- src/config/imageBuilder.ts
+++ src/config/imageBuilder.ts
@@ -1,17 +1,11 @@
-"use client"
+
 import imageUrlBuilder from "@sanity/image-url";
-import { ClientConfig, createClient } from "next-sanity";
+
+import { client } from "./sanityClient";
 
 
-const config: ClientConfig = {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    apiVersion: "2022-03-07",
-    useCdn: true,
-    token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
-  };
-  
-const client = createClient(config);
+
+
 const builder = imageUrlBuilder(client);
 
 export const imageUrlFor = (source: string) => builder.image(source);
